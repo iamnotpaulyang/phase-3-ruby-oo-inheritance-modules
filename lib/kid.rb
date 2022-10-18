3aa8be076eb3890b250a6186aab9813f85967f05
Paul Yang <paulyang00@gmail.com>
2022-10-18 01:29:55 -0400

    commit tree 0c6cd6fe4134fee183eb4034770135292d96e1f0

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,23 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+
+# class Kid
+#     include Dance
+#     extend MetaDancing
+#     attr_accessor :name
+
+#     def initialize(name)
+#         @name = name
+#     end
+# end
+
+require_relative './fancy_dance'
+class Kid
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
